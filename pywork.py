c1b9cfe05f95969c23c9f7935d99f96647fd53cf
pranavletsbuild <pranav.softwareai@gmail.com>
2026-08-10 18:37:48 +0530

    This is handling the exception

--- pywork.py
+++ pywork.py
@@ -1,3 +1,6 @@
 import sys
 
-print("Hi my name is:",sys.argv[1])
+try:
+    print("Hi my name is:", sys.argv[1])
+except IndexError:
+    print("Please provide your name as a command-line argument.")
