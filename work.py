86e97e2be61f6302af771f1004aa25fe5a4d44ec
pranavletsbuild <pranav.softwareai@gmail.com>
2026-08-18 22:08:13 +0530

    introduced classes and objects.

--- work.py
+++ work.py
@@ -1,10 +1,12 @@
-def check_num(num):
-    if num>0:
-        print("positive")
-    elif num<0:
-        print("Negative")
-    else:
-        print("Zero")
+class NumberCheck:
+    def check_num(self,num):
+        if num>0:
+            print("positive")
+        elif num<0:
+            print("Negative")
+        else:
+            print("Zero")
 
-num =int(input("Enter a number:"))
-check_num(num)
+checker=NumberCheck()
+checker.check_num(num)
+
